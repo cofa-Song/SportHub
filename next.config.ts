6fa89b7eeb4e9a64b4e2aec3383754e18f560f5a
Cofa <Cofa_@aerixc.com>
2026-02-26 12:02:07 +0800

    update

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export', // 這行會讓 Next.js 輸出靜態 HTML/CSS/JS
+  basePath: '/SportHub', // GitHub 倉庫名稱
+  assetPrefix: '/SportHub/', // 確保 CSS/JS 檔案路徑正確
   images: { unoptimized: true }, // GitHub Pages 不支援 Next 的圖片優化
 };
 
